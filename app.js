0dbe9b8c9b7a2bd3fca2f7de2702ce1a4082ee32
test <leejeayoung875@gmail.com>
2024-06-18 12:23:41 +0900

    서버완료

--- app.js
+++ app.js
@@ -11,6 +11,40 @@ const server = http.createServer((request,response)=>{
       response.end();
     }
   }
+  if(request.method === "POST") {
+    console.log("dd");
+    if(request.url === "/login") {
+      let body = "";
+      request.on ("data",(data)=> {
+        body += data;
+        console.log(body);
+      });
+      request.on("end",()=>{
+        response.setHeader('Content-Type',"text/html",'charset=utf8')
+        response.writeHead(200);
+        const data = body;
+        console.log(data)
+        console.log(body)
+  
+        const html=`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database CRUD Test</title>
+  </head>
+  <body>
+    <div>${data}</div>
+  </body>
+  </html>
+  `
+        response.write(html);
+        response.end();
+
+      });
+      // const index = fs.readFileSync("./public/html/index.html")
+    }
+  }
 })
 
 server.listen(3000);
